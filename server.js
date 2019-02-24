9c3da6ae6e8ab841fb33807a5d828c4c6085e6e5
Aneesh Kapur <aneesh.kapur@cognizant.com>
2019-02-24 20:41:23 +0530

    new feature

--- server.js
+++ server.js
@@ -71,6 +71,13 @@ res.render('about.hbs', {
 });
 
 });
+app.get('/projects', (req,res) => {
+
+  res.render('projects.hbs',{
+
+    pageTitle: 'Projects Page'
+  });
+});
 
 app.get('/bad',(req,res) => {
 
